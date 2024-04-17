632ba1a5c6fe15eac96f60055f1edbc53ab7591a
Christian Viñas <cnvinas2@gmail.com>
2024-04-17 16:13:03 -0300

    added prettier

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -1,36 +1,35 @@
-import Spinner from "../UI/Spinner";
+import Spinner from '../UI/Spinner'
 
-import useFetch from "../../utils/useFetch";
+import useFetch from '../../utils/useFetch'
 
-export default function UsersList ({user, setUser}) {
+export default function UsersList({ user, setUser }) {
+  const {
+    data: users = [],
+    status,
+    error,
+  } = useFetch('http://localhost:3001/users')
 
-  const {data: users = [], status, error} = useFetch(
-    "http://localhost:3001/users"
-  );
-
-  if (status === "error") {
+  if (status === 'error') {
     return <p>{error.message}</p>
   }
 
-  if (status === "loading") {
-    return <p><Spinner/> Loading users...</p>
+  if (status === 'loading') {
+    return (
+      <p>
+        <Spinner /> Loading users...
+      </p>
+    )
   }
 
   return (
     <ul className="users items-list-nav">
-      {users.map(u => (
-        <li
-          key={u.id}
-          className={u.id === user?.id ? "selected" : null}
-        >
-          <button
-            className="btn"
-            onClick={() => setUser(u)}
-          >
+      {users.map((u) => (
+        <li key={u.id} className={u.id === user?.id ? 'selected' : null}>
+          <button className="btn" onClick={() => setUser(u)}>
             {u.name}
           </button>
         </li>
       ))}
     </ul>
-  );
+  )
 }
