286899eee3094e68e494981447f88b9b86031f25
Christian Viñas <cnvinas2@gmail.com>
2024-05-07 13:43:37 -0300

    sale con errores

--- src/components/Users/UserPicker.spec.js
+++ src/components/Users/UserPicker.spec.js
@@ -1,85 +1,40 @@
-import React from 'react';
-import useFetch from '../../utils/useFetch';
-import App from '../App';
-import { renderHook } from '@testing-library/react-hooks';
-import { render } from '@testing-library/react';
-
-
-// Mock the fetchData function
-jest.mock('../../utils/useFetch');
+/* eslint-disable react/display-name */
+/* eslint-disable no-undef */
+import '@testing-library/jest-dom'
+import React from 'react'
+import App from '../App'
+import { render, screen } from '@testing-library/react'
+import 'babel-polyfill'
 
 // Mock the spinner component
-jest.mock('../UI/Spinner', () => () => <div data-testid="spinner">Mocked Spinner</div>);
+jest.mock('../UI/Spinner', () => () => (
+  <div data-testid="spinner">Mocked Spinner</div>
+))
 
-describe('UserPicker', () => {
-  describe('Loading Status', () => {
-    it('renders spinner when network request is in progress', () => {
-      const { getByTestId } = render(<App />);
-      //todo 
-      //mock get data para retrasar el status del loading
-      //usefetch y getdata
-      const spinner = getByTestId('spinner');
-      expect(spinner).toBeInTheDocument;
-    });
-
-    /*it('renders spinner when network request is done', () => {
-      
-    });*/
-  })
-
-  it.only('renders users content when isLoading is false', () => {
-    // Mock useState to set isLoading to false
-    useFetch.mockReturnValue({data: [{id:1, name: 'chris'}], status: 'done'});
-    const { queryByTestId } = render(<App />);
-    const spinner = queryByTestId('spinner');
-    expect(spinner).toBeNull;
-  });
-
-});
-/*describe('useFetch', () => {
-  it('fetches data successfully', async () => {
-    // Mock data to be returned by the fetch call
-    const mockData = {                                                                                                                    
-      data: {users:[1]},
-      status: 'done'
-    };
-    
-    // Mock the global fetch function
-    global.fetch = jest.fn().mockResolvedValueOnce({
-      json: jest.fn().mockResolvedValueOnce(mockData),
-    });
-
-    // Render the hook
-    const { result, waitForNextUpdate } = renderHook(() => useFetch('http://localhost:3001/users'));
-
-    // Wait for the hook to fetch data
-    await waitForNextUpdate();
-
-    console.log(result.current)
-
-    // Assertions
-    expect(result.current.loading).toBe(false); // Ensure loading state is false
-    expect(result.current.error).toBe(null); // Ensure no error occurred
-    expect(result.current.data).toEqual(mockData); // Ensure fetched data is correct
-  });
-});*/
-
-
-// Mockear la función fetch global
-/*global.fetch = jest.fn(() =>
+global.fetch = jest.fn(() =>
   Promise.resolve({
-    json: () => Promise.resolve({ data: users = [], status: 'done' }),
-  })
-);
+    json: () =>
+      Promise.resolve([
+        { id: 1, name: 'User 1' },
+        { id: 2, name: 'User 2' },
+      ]),
+  }),
+)
 
 describe('UserPicker', () => {
-    test('should fetch data from API', async () => {
-        const url = "http://localhost:3001/users";        
-        // Renderizar el custom hook
-        fetch.mockImplementationOnce(()=> Promise.resolve());
-        
-        console.log(context)
-        // Verificar que el resultado del custom hook es el esperado
-        expect(fetch).toHaveBeenCalledWith("http://localhost:3001/users");
-      });
-})*/
+  it('renders spinner when network request is in progress', () => {
+    const { getByTestId } = render(<App />)
+
+    expect(screen.queryByText('Loading...')).toBeNull()
+    expect(screen.getByText('User 1')).toBeInTheDocument()
+
+    // select a different user
+    const selectElement = screen.getByTestId('user-picker')
+    userEvent.selectOptions(selectElement, '2')
+
+    expect(screen.getByText('User 2')).toBeInTheDocument()
+    //usefetch y getdata
+    const spinner = getByTestId('spinner')
+    expect(spinner).toBeInTheDocument
+  })
+})
